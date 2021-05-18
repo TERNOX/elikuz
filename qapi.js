ded487a44b0e0cfdd2c5d05cf73ddf7341ca39ce
TERNOX <43003712+TERNOX@users.noreply.github.com>
2021-05-19 00:15:05 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60a42e2413e4b0001f82a130",
+				"60a42e2413e4b0001f82a132"
+			]
+		},
+		"60a42e2413e4b0001f82a130": {
+			"id": "60a42e2413e4b0001f82a130",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60a42e2413e4b0001f82a132": {
+			"id": "60a42e2413e4b0001f82a132",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
