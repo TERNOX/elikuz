02671bc1816d319e76f6bed72745aed9dca1a2bd
TERNOX <43003712+TERNOX@users.noreply.github.com>
2021-05-19 01:04:55 +0300

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Text } from "@quarkly/widgets";
+import { Theme, Link, Text, Icon, Box, Input, Button } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
-import { RawHtml, Override, Menu, Section } from "@quarkly/components";
+import { RawHtml, Section, Override, StackItem, Stack, Formspree } from "@quarkly/components";
+import { MdLocationOn, MdEmail, MdPhone } from "react-icons/md";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
@@ -15,49 +16,134 @@ export default (() => {
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
 		<Section>
-			<Text margin="0px 0 24px 0" text-align="center" font="--headline2" md-font="--headline3">
+			<Text margin="0px 0 24px 0" text-align="center" font="--headline1" md-font="--headline3">
 				Elikuz Media
 			</Text>
-			<Menu
-				display="flex"
-				justify-content="center"
-				font="--base"
-				font-weight="700"
-				md-flex-direction="column"
-				md-align-items="center"
-			>
-				<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
-				<Override slot="link-active" color="--primary" />
-				<Override slot="item" padding="6px" />
-			</Menu>
+			<Text margin="0px 0 24px 0" text-align="center" font="--headline3" md-font="--headline3">
+				Elisey Kuziakin
+			</Text>
 		</Section>
 		<Section
-			text-align="center"
-			background-color="--primary"
 			color="--light"
 			padding="100px 0"
 			sm-padding="40px 0"
+			position="relative"
+			background="linear-gradient(0deg,rgba(25, 30, 34, 0.8) 0%,rgba(25, 30, 34, 0.8) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1547619292-240402b5ae5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80)"
 		>
-			<Text
-				as="h5"
-				font="--lead"
-				margin="10px 0 0 0"
-				text-transform="uppercase"
-				letter-spacing="5px"
-			>
-				PRODUCTION
-			</Text>
-			<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0 0 0">
-				About Us
-			</Text>
-			<Text as="p" font="--lead" margin="40px 0 20px 0">
-				Hello!
-			</Text>
-		</Section>
-		<Section>
-			<Text margin="0px 0 24px 0" text-align="center" font="--headline3" md-font="--headline3">
-				contact@elikuz.com
-			</Text>
+			<Stack>
+				<StackItem width="50%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="32px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdLocationOn}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Visit us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							Uusikatu 72, A 22
+Oulu
+Pohjois-Pohjanmaa
+Finland
+90120
+						</Text>
+					</Box>
+					<Box
+						padding="0 0 0 64px"
+						sm-padding="64px 0 0 0"
+						margin="64px 0 0 0"
+						max-width="360px"
+						position="relative"
+					>
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdEmail}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Email us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							<Link href="mailto:hello@company.com" text-decoration="none" hover-text-decoration="underline" color="--light">
+								contact@elikuz.com
+							</Link>
+						</Text>
+					</Box>
+					<Box padding="0 0 0 64px" margin="64px 0 0 0" max-width="360px" position="relative">
+						<Icon
+							position="absolute"
+							size="48px"
+							top="0"
+							left="0"
+							category="md"
+							icon={MdPhone}
+						/>
+						<Text as="h4" margin="6px 0" font="--base">
+							Call us
+						</Text>
+						<Text as="p" margin="6px 0" font="--headline3">
+							+358465553684
+						</Text>
+					</Box>
+				</StackItem>
+				<StackItem width="50%" md-width="100%">
+					<Box
+						max-width="360px"
+						padding="56px 48px"
+						margin="0 0 0 auto"
+						md-max-width="100%"
+						background="--color-primary"
+					>
+						<Text as="h3" font="--headline3" margin="0 0 20px 0">
+							Leave us message
+						</Text>
+						<Formspree endpoint="https://formspree.io/f/mwkalapg">
+							<Stack gap="16px">
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Name
+									</Text>
+									<Input width="100%" name="name" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Email
+									</Text>
+									<Input width="100%" type="email" name="email" />
+								</StackItem>
+								<StackItem width="100%">
+									<Override slot="StackItemContent" flex-direction="column" />
+									<Text font="--base" margin="0 0 4px 0">
+										Message
+									</Text>
+									<Input as="textarea" rows="4" width="100%" name="message" />
+								</StackItem>
+								<StackItem width="100%">
+									<Button background="--color-dark">
+										Send
+									</Button>
+								</StackItem>
+							</Stack>
+						</Formspree>
+					</Box>
+				</StackItem>
+			</Stack>
 		</Section>
 		<Link
 			font={"--capture"}
